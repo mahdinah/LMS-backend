ef8db42a466955b009a28e2f631994280420e2db
mahdi halloum <mahdinah98@gmail.com>
2023-03-23 09:19:49 +0200

    final touch

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,57 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Student;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
+
 
 class StudentController extends Controller
 {
 
     public function createStudent(Request $request)
-{
-    $student = new Student();
-
-    $student->fname = $request->input('fname');
-    $student->lname = $request->input('lname');
-    $student->fathername = $request->input('fathername');
-    $student->mothername = $request->input('mothername');
-    $student->gender = $request->input('gender');
-    $student->dateofbirth = $request->input('dateofbirth');
-    $student->email = $request->input('email');
-    $student->pnumber = $request->input('pnumber');
-    $student->address = $request->input('address');
-    $student->image = $request->input('image');
-    $student->HealthProblems = $request->input('HealthProblems');
-    $student->bloodType = $request->input('bloodType');
-    $student->section_id = $request->input('section_id');
-
-    $student->save();
-
-    // Create new student instance with validated data
-    // $student = new Student($validatedData);
-
-    // Upload and save student image if it exists
-    if ($request->hasFile('image')) {
-        $image = $request->file('image');
-        $name = time() . '.' . $image->getClientOriginalExtension();
-        $imageResized = Image::make($image)->resize(400, null, function ($constraint) {
-            $constraint->aspectRatio();
-        })->encode('jpg', 80);
-        Storage::disk('public')->put('images/' . $name, $imageResized);
-        $student->image = $name;
+    {
+        $data = new Student;
+        try {
+            if ($request->image && $request->image !== "") {
+                $image = $request->image;
+                $name = time() . '.' . explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];
+                $img = Image::make($image)->resize(400, null, function ($constraint) {
+                    $constraint->aspectRatio();
+                });
+                $img->save(storage_path('app/public/images/') . $name);
+            }
+            $data->fill($request->all());
+            $data->image = $name;
+            $data->save();
+            return [
+                'success' => true,
+                'data' => $data,
+            ];
+        } catch (Exception $e) {
+            return false;
+        }
     }
-
-    // Save student to database
-    $student->save();
-
-    // Return success response with saved student data
-    return [
-        'success' => true,
-        'message' => 'Student created successfully',
-        'data' => $student,
-    ];
-}
-    
 
     public function getStudents()
     {
@@ -88,21 +66,32 @@ class StudentController extends Controller
         $data = Student::where('id', $id)->first();
 
         try {
-            if ($request->has('image') && $request->image->isValid()) {
+            if ($request->image && $request->image !== $data->image) {
                 $image = $request->image;
-                $name = time() . '.' . $image->getClientOriginalExtension();
-                $imageResized = Image::make($image)->resize(400, null, function ($constraint) {
+                $name = time() . '.' . explode('/', explode(':', substr($image, 0, strpos($image, ';')))[1])[1];
+                $image = Image::make($image);
+                $image->resize(400, null, function ($constraint) {
                     $constraint->aspectRatio();
-                })->encode('jpg', 80);
-                Storage::disk('public')->put('images/' . $name, $imageResized);
-                if ($data->image && Storage::disk('public')->exists('images/' . $data->image)) {
-                    Storage::disk('public')->delete('images/' . $data->image);
+                });
+                $image->save(storage_path('app/public/images/') . $name);
+                if (file_exists(storage_path('app/public/images/') . $data->image)) {
+                    unlink(storage_path('app/public/images/') . $data->image);
                 }
-                $data->image = $name;
+                $data->update(['image' => $name]);
             }
 
-            $data->fill($request->all());
-            $data->save();
+            $data->update(['fname' => $request->fname]);
+            $data->update(['lname' => $request->lname]);
+            $data->update(['fathername' => $request->fathername]);
+            $data->update(['mothername' => $request->mothername]);
+            $data->update(['gender' => $request->gender]);
+            $data->update(['dateofbirth' => $request->dateofbirth]);
+            $data->update(['email' => $request->email]);
+            $data->update(['pnumber' => $request->pnumber]);
+            $data->update(['address' => $request->address]);
+            $data->update(['HealthProblems' => $request->HealthProblems]);
+            $data->update(['bloodType' => $request->bloodType]);
+            $data->update(['section_id' => $request->section_id]);
             return [
                 'success' => true,
                 'data' => $data,
@@ -116,17 +105,18 @@ class StudentController extends Controller
     {
         try {
             $data = Student::where('id', $id)->first();
-            if ($data->image && Storage::disk('public')->exists('images/' . $data->image)) {
-                Storage::disk('public')->delete('images/' . $data->image);
+            if ($data->image && (file_exists(storage_path('app/public/images/') . $data->image))) {
+                unlink(storage_path('app/public/images/') . $data->image);
             }
             $data = Student::where('id', $id)->delete();
             return [
-                'success' => true,
-                'data' => $data,
+            'success' => true,
+            'data' => $data,
             ];
-        } catch (Exception $e) {
+            } catch (Exception $e) {
             return false;
-        }
-    }
-
-}
+            }
+            }
+            
+            } // end of class
+            ?>
